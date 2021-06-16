b245a6af6923a09557dd2b43b4d269c112ed02fd
Mayur Wankhade <mayur.wankhade2@gmail.com>
2021-06-16 19:18:50 +0530

    [Mayur] Add : First Name validation method added.

--- UserRegistrationProblem/UserRegistrationProblem/Program.cs
+++ UserRegistrationProblem/UserRegistrationProblem/Program.cs
@@ -4,9 +4,12 @@ namespace UserRegistrationProblem
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("Welcome To User Registration Problem!");
+
+            RegexTest R = new RegexTest();
+            Console.WriteLine(R.TestFirstName("Mayur"));
         }
     }
 }
---
+++ UserRegistrationProblem/UserRegistrationProblem/RegexTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationProblem
+{
+    class RegexTest
+    {
+        public string firstName = "^[A-Z]{1}[a-zA-z]{2,10}";
+
+        public bool TestFirstName(string fName)
+        {
+            return Regex.IsMatch(fName, firstName);
+        }
+    }
+}
